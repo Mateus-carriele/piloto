68c71e15f66ff3126cddb1b6bee34c55f35e425f
Mateus-carriele <160273858+Mateus-carriele@users.noreply.github.com>
2024-11-15 21:09:39 -0300

    ss

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),                         # Página inicial
     path('exibir_item/<int:id>/', views.exibir_item, name="exibir_item"),  # Exibir item específico
-    path('dia_semana/<int:dia>/', views.dia_semana, name="dia_semana"),    # Exibir dia da semana
+    path('dia_semana/<int:dia>/', views.dia_semana, name="dia_semana"),# Exibir dia da semana
+    path('produtos/', views.produtos, name='produtos'),   
+    path('produtos/fomulario_contato/', views.contato, name='fomulario'),
+    path('produtos/fomulario_produto/', views.produto, name='fomulario2'), 
+    path('produtos/excluir/<int:id>/', views.excluir_produto, name='excluir_produto'),     
 ]
